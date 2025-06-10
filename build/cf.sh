ddf9b4aac8da8d6a86b6c9870ef5e239ac255fd0
Nihar Kodkani <nihar.kodkani@gmail.com>
2025-06-10 16:36:02 +0000

    updated config script

--- build/cf.sh
+++ build/cf.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 ../configure \
 	--prefix=$(pwd)/install \
-	--with-device=ch4:ucx \
-	--with-ucx=/home/nkodkani/ucx/build/install \
-	--with-hip=/soft/compilers/rocm/rocm-6.3.2 \
-	--with-ucc=/home/nkodkani/ucc/build/install \
-	LDFLAGS="-Wl,-rpath,/home/nkodkani/ucx/build/install/lib -Wl,-rpath,/soft/compilers/rocm/rocm-6.3.2/lib"
+	--with-device=ch4:ofi \
+	--with-libfabric=embedded \
+	--with-cuda=/soft/compilers/cuda/cuda-12.3.0 \
+	--with-ucc=$HOME/ucc/build/install \
+	--enable-g=all |& tee configure.out
